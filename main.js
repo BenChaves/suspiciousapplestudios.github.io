46664c1a4c8dd81db6637b8485e6023b3f077a11
BenChaves <greenlegoman08@gmail.com>
2024-10-06 22:34:14 -0400

    Player Movement and submission video and build

--- main.js
+++ main.js
@@ -8,20 +8,30 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.setZ(30);
+camera.position.setZ(7);
 //moveCamera()
 
 //GEOMETRY
 const geometry = new THREE.DodecahedronGeometry(10, 0);
-const material = new THREE.MeshStandardMaterial({ color: 0x39fc03 });
+const material = new THREE.MeshStandardMaterial({ color: 0x000000 });
 const die = new THREE.Mesh(geometry, material);
+
+const edges = new THREE.EdgesGeometry(geometry, 1);
+const edgeMat = new THREE.LineBasicMaterial({color: 0x39fc03});
+const dieWire = new THREE.LineSegments(edges, edgeMat);
+
+scene.add(dieWire)
+
+die.scale.set(0.99, 0.99, 0.99)
+
+//die.scale.set(0.8, 0.8, 0.8)
 
 scene.add(die)
 
@@ -50,17 +60,17 @@ function addStar() {
   const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100)*2);
 
   star.position.set(x,y,z);
   scene.add(star)
 }
 
-Array(200).fill().forEach(addStar) //add stars :D
+//Array(200).fill().forEach(addStar) //add stars :D
 
 //BACKGROUND
 const bgTex = new THREE.TextureLoader().load('space.jpg');
-scene.background = bgTex;
+//scene.background = bgTex;
 
 
 
@@ -90,9 +100,13 @@ function animate(){
   requestAnimationFrame(animate);
   //begin of render loop
 
-  die.rotation.x += 0.004; //rotate die
-  die.rotation.y += 0.004;
+  dieWire.rotation.x += 0.001; //rotate die
+  dieWire.rotation.y += 0.001;
+  dieWire.rotation.y += 0.0009;
+
+  die.rotation.x += 0.001; //rotate die
   die.rotation.y += 0.001;
+  die.rotation.y += 0.0009;
 
   //controls.update(); //may not be needed
 
